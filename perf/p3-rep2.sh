c3bbef5c60b63bde2c8e157de8104d7102df88e5
Yifeng Jiang <uprushworld@gmail.com>
2020-02-03 13:20:16 +0800

    enable leader auto rebalance

--- perf/p3-rep2.sh
+++ perf/p3-rep2.sh
@@ -3,6 +3,12 @@
 echo "Delete existing logs."
 rm -f ../logs/kafka-perf-*.log
 
+echo "Delete pre-run kafka perf client"
+for NO in {1..3}
+do
+    kubectl delete pods kafka-perf-$NO --grace-period=0 --force
+done
+
 echo "Creating Kafka perf test pods."
 kubectl create -f ../kafka-perf-r3.yaml
 
